60ebe6fd136ca750cd0bc2c58b982949b9050bd6
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-26 16:47:46 +0100

    finished

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,15 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var age = new Date(1995,5,7);
+var futurAge = new Date(2026,5,7);
+
+//futurAge -= age;
+var birthDate = myFunction(futurAge, age);
+function myFunction(futurAge , age){
+    return futurAge.getFullYear() - age.getFullYear()
+}
+
+console.log('In june 7 2026 you will be',birthDate);
+ 
