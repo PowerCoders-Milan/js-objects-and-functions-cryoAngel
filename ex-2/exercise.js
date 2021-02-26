2bacd6b4e55065bb4682a5be09859f031eef5229
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-26 12:30:57 +0100

    finished

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,13 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function fortuneTeller(children, partner, geographicLocation, jobTitle){
+    this.children = children;
+    this.partner = partner;
+    this.geographicLocation = geographicLocation;
+    this.jobTitle = jobTitle;
+}
+
+var fortuneTeller = new fortuneTeller(prompt('number of children'), prompt('name of partner'), prompt('location'), prompt('job title'));
+window.alert(`You will be a ${fortuneTeller.jobTitle} in ${fortuneTeller.geographicLocation} , and married to ${fortuneTeller.partner} with ${fortuneTeller.children} kids.`)
+
