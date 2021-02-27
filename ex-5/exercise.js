218b6c5cfcf3f9ba394f754a70cb4a2031d35435
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-27 11:16:38 +0100

    finished

---
+++ ex-5/exercise.js
@@ -0,0 +1,25 @@
+
+
+
+  var text;
+  var words = prompt('HelloWorld',"english" );
+  switch(words) {
+    case "english":
+      text = "Hello world";
+      break;
+    case "french":
+      text = "salut tous le monde";
+      break;
+    case "russian":
+      text = "privet mir";
+      break;
+    default:
+      text = "hello world";
+  }
+  function helloWorld(){
+
+  }
+  window.alert(text);
+  console.log(text);
+
+
